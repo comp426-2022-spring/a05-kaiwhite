1aaed40ce93219ac1573ed8df6fe4b872b90f177
Kai White <kaiwhite@live.unc.edu>
2022-04-30 12:12:19 -0400

    fix update

--- src/controllers/mycontrollers.js
+++ src/controllers/mycontrollers.js
@@ -1,1 +1,94 @@
 // The files in this directory contain functions that handle requests coming to different routes
+
+
+/** Coin flip functions 
+ * This module will emulate a coin flip given various conditions as parameters as defined below
+ */
+
+/** Simple coin flip
+ * 
+ * Write a function that accepts no parameters but returns either heads or tails at random.
+ * 
+ * @param {*}
+ * @returns {string} 
+ * 
+ * example: coinFlip()
+ * returns: heads
+ * 
+ */
+
+ function coinFlip() {
+    return Math.floor(Math.random() * 2) == 0 ? "tails" : "heads"
+  }
+  
+  /** Multiple coin flips
+   * 
+   * Write a function that accepts one parameter (number of flips) and returns an array of 
+   * resulting "heads" or "tails".
+   * 
+   * @param {number} flips 
+   * @returns {string[]} results
+   * 
+   * example: coinFlips(10)
+   * returns:
+   *  [
+        'heads', 'heads',
+        'heads', 'tails',
+        'heads', 'tails',
+        'tails', 'heads',
+        'tails', 'heads'
+      ]
+   */
+  
+  function coinFlips(flips) {
+    if (flips < 1 || typeof flips ==="undefined"){
+      flips = 1;
+    }
+    const results = [];
+    for(var i = 0; i < flips; i++) {
+      results.push(coinFlip());
+    }
+    return results;
+  }
+  
+  /** Count multiple flips
+   * 
+   * Write a function that accepts an array consisting of "heads" or "tails" 
+   * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+   * an object containing the number of each.
+   * 
+   * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
+   * { tails: 5, heads: 5 }
+   * 
+   * @param {string[]} array 
+   * @returns {{ heads: number, tails: number }}
+   */
+  
+  function countFlips(array) {
+    let hNum = 0;
+    let tailCount = 0;
+    for (var i = 0; i < array.length; i++) {
+      if(array[i].localeCompare('heads')) {
+        hNum++;
+      } else {
+        tailCount++;
+      } // we can assume only heads or tails as we are providing the array ourselves
+    }
+    return {heads: tailCount,tails: hNum};
+  }
+  
+  /** Flip a coin!
+   * 
+   * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
+   * 
+   * @param {string} call 
+   * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
+   * 
+   * example: flipACoin('tails')
+   * returns: { call: 'tails', flip: 'heads', result: 'lose' }
+   */
+  
+  function flipACoin(call) {
+    let flip = coinFlip();
+    return {call: call, flip: flip, result: flip == call ? "win" : "lose" }
+  }
--- src/services/database.js
+++ src/services/database.js
@@ -1,1 +1,23 @@
 // Put your database code here
+const Database = require('better-sqlite3')
+
+const db = new Database('log.db')
+
+
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog'`)
+let row =  stmt.get();
+// this checks if the database exists and creates one if it does not
+if (row === undefined) {
+    console.log('log database is missing. Creating log database.')
+    const sqlInit = `
+        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url TEXT, protocol TEXT, httpversion TEXT, status TEXT, referrer TEXT, useragent TEXT );
+        `
+        
+    db.exec(sqlInit)
+    
+    console.log("Your database has been created")
+} else {
+    console.log('Log database exists')
+}
+
+module.exports = db
